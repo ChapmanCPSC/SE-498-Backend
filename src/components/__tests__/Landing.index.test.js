a6055780ef44e2871d4e354a1da52694468bae3b
melindasherrill <sherr107@mail.chapman.edu>
2018-03-21 10:27:04 -0700

    Update Landing.index.test.js

--- src/components/__tests__/Landing.index.test.js
+++ src/components/__tests__/Landing.index.test.js
@@ -1,3 +1,10 @@
 
 import React from 'react';
 import index from '../Landing/index'
+
+import renderer from 'react-test-renderer';
+
+test('landing page renders correctly', () => {
+  const tree = renderer.create(<index />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
