cd09fe30b8db2a50c8e9753a9009a78b6082c879
melindasherrill <sherr107@mail.chapman.edu>
2018-03-22 16:11:20 -0700

    Create App.index.test.js

---
+++ src/components/__tests__/App.index.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import index from '../App/index'
+
+import renderer from 'react-test-renderer';
+
+test('landing page renders correctly', () => {
+  const tree = renderer.create(<index />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
